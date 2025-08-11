5c0d260a7a78a0e3e7846685210cbdf0a13eb6da
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2025-08-11 21:00:00 +0200

    add sidebar and footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2'>Footer</footer>
-  )
-}
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="flex-shrink-0">
+          <Image
+            src="hr.svg"
+            alt="Croatian"
+            width={32}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size="lg" variant="ghost" className="flex-shrink-0">
+          <Image
+            src="es.svg"
+            alt="Spanish"
+            width={32}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="flex-shrink-0">
+          <Image
+            src="fr.svg"
+            alt="French"
+            width={32}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="flex-shrink-0">
+          <Image
+            src="it.svg"
+            alt="Italian"
+            width={32}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button size="lg" variant="ghost" className="flex-shrink-0">
+          <Image
+            src="jp.svg"
+            alt="Italian"
+            width={32}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
