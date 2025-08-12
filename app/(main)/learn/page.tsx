9f9729c0e57d47ef7b374d25ef533ac6dda462be
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2025-08-12 12:21:55 +0200

    add drizzle configuration and models

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,23 +3,44 @@ import { StickyWrapper } from '@/components/sticky-wrapper';
 import React from 'react';
 import { Header } from './header';
 import { UserProgress } from '@/components/user-progress';
+import { getUnits, getUserProgress } from '@/db/queries';
+import { redirect } from 'next/navigation';
 
-const page = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const unitsData = getUnits();
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses');
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: 'Spanish', imgSrc: '/es.svg' }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
+        {units.map(unit => (
+          <div key={unit.id}>
+            <h2>{unit.title}</h2>
+            {unit.lessons.map(lesson => (
+              <div key={lesson.id}>{lesson.title}</div>
+            ))}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
 };
 
-export default page;
+export default LearnPage;
---
+++ scripts/seed.ts
@@ -0,0 +1,107 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+import * as schema from '@/db/schema';
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log('Seeding database');
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: 'Spanish',
+        imageSrc: '/es.svg',
+      },
+      {
+        id: 2,
+        title: 'French',
+        imageSrc: '/fr.svg',
+      },
+      {
+        id: 3,
+        title: 'Croatian',
+        imageSrc: '/hr.svg',
+      },
+      {
+        id: 4,
+        title: 'Italian',
+        imageSrc: '/it.svg',
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: 'Unit 1',
+        description: 'Learn the basics of Spanish',
+        order: 1,
+      },
+    ]);
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: 'Nouns',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        question: 'Which one of these is the "the man"?',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: '/man.svg',
+        text: 'el hombre',
+        correct: true,
+        audioSrc: '/es_man.mp3',
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: '/woman.svg',
+        text: 'la mujer',
+        correct: false,
+        audioSrc: '/es_woman.mp3',
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: '/robot.svg',
+        text: 'el robot',
+        correct: false,
+        audioSrc: '/robot.mp3',
+      },
+    ]);
+
+    console.log('Seeding finished');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to seed the database');
+  }
+};
+
+main();
