6a307b7e3b550962cdb1e33db29c691d29d430fd
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2025-08-19 16:43:39 +0200

    add configuration to vercel

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,20 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  experimental: {
+    optimizePackageImports: ['lightningcss'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
