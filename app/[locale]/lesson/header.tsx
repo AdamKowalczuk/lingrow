2e4f8c97bd044e917fcbc4e81680293a800c8dc3
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2025-08-25 16:58:17 +0200

    fix header on mobile

--- app/[locale]/lesson/header.tsx
+++ app/[locale]/lesson/header.tsx
@@ -18,7 +18,7 @@ const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
   const { open } = useExitModal();
   const t = useTranslations('images');
   return (
-    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
+    <header className="lg:pt-[50px] pt-[20px] px-6 lg:px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <button
         onClick={open}
         className="text-gray-500 hover:text-gray-700 hover:bg-gray-100 p-2 rounded-lg transition-all duration-200 cursor-pointer"
@@ -29,18 +29,18 @@ const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
         value={percentage}
         className="w-full h-3 bg-gray-200 rounded-full overflow-hidden"
       />
-      <div className="text-rose-500 flex items-center font-bold bg-rose-50 px-3 py-2 rounded-lg">
+      <div className="flex items-center font-bold bg-rose-50 px-3 py-2 rounded-lg">
         <Image
           src="/heart.svg"
           height={24}
           width={24}
           alt={t('hearts')}
-          className="mr-2"
+          className="mr-2 flex-shrink-0"
         />
         {hasActiveSubscription ? (
-          <InfinityIcon className="h-5 w-5 stroke-[2.5] shrink-0" />
+          <InfinityIcon className="h-5 w-5 stroke-[2.5] flex-shrink-0" />
         ) : (
-          <span className="text-rose-600">{hearts}</span>
+          <span className="text-rose-600 flex-shrink-0">{hearts}</span>
         )}
       </div>
     </header>
